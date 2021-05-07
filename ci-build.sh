5e4bb3702144723d3ffeabda9fa7e0c3fa205748
RS <sayrer@gmail.com>
2021-05-07 11:13:15 -0700

    run the binary

--- ci-build.sh
+++ ci-build.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
-set -v
-set -e
+#!/bin/bash
 
 xcodebuild -scheme $SCHEME build -destination "$DESTINATION" -showBuildTimingSummary
+x=$( xcodebuild -showBuildSettings -project MacPlatformTest.xcodeproj | grep ' BUILD_DIR =' | sed -e 's/.*= *//' )
+
+DYLD_FRAMEWORK_PATH=$x/Debug DYLD_LIBRARY_PATH=$x/Debug $x/Debug/MacPlatformTest
