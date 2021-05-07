c33b6ededc61899e964bb77fe7d174dfb96dff69
RS <sayrer@gmail.com>
2021-05-07 11:17:45 -0700

    Split up tasks.

--- ci-build.sh
+++ ci-build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
 xcodebuild -scheme $SCHEME build -destination "$DESTINATION" -showBuildTimingSummary
-x=$( xcodebuild -showBuildSettings -project MacPlatformTest.xcodeproj | grep ' BUILD_DIR =' | sed -e 's/.*= *//' )
-
-DYLD_FRAMEWORK_PATH=$x/Debug DYLD_LIBRARY_PATH=$x/Debug $x/Debug/MacPlatformTest
